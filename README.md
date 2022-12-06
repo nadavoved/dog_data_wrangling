b7767d7521ccb8bc8a4d5fa1e25279025c523bdd
Nadav Oved <nadavove@gmail.com>
2022-12-06 17:23:00 +0200

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# TO BE ADDED...
+# USER GUIDE
+___
+## Overview
+This is a data wrangling and analysis project of WeRateDogs tweeter archive.
+___
+## Getting Started
+**Verify Python Version - 3.11**
+1. *Required Setup(Command Line)*
+   1. `cd <project root>`
+   2. `python3.11 -m venv venv`
+   3. `./venv/Scripts/activate`
+   4. `pip install -r requirements.txt`
+   5. `jupyter notebook`
+2. *Within Jupyter Notebook:*
+   - open `wrangle_act.ipynb` for step-by-step walkthrough of the entire process. 
+   - More Info
+     - open `gather.py` for detailed code and docstring of gathering functions.
+     - view `wrangle_report.md` for wrangling report.
+     - view `act_report.md` for analysis report.
+___
+
+
+
+
+
+
+
--- gather.py
+++ gather.py
@@ -1,11 +1,17 @@
+"""Gathering functions."""
+
 import json
 
 import requests
-import pandas as pd
 import tweepy
 
 
 def download_file_from_url(url_path, destination):
+    """Save File From a given url.
+
+    :param url_path: (str) a path a file containing the url.
+    :param destination: (str) a path to save the response at.
+    """
     with open(url_path) as file:
         url = file.read()
     response = requests.get(url)
@@ -14,31 +20,42 @@ def download_file_from_url(url_path, destination):
         file.write(response.content)
 
 
-def get_tweeter_api():
-    with open('auth.txt') as file:
+def get_auth(auth_file):
+    """Return authentication from params in auth file.
+
+    See README.md for how to format auth file.
+    :param auth_file: (str) path to auth file
+    :return: (tweepy.auth.OAuth1UserHandler) an auth object.
+    """
+    with open(auth_file) as file:
         args = [line.split('=')[1].rstrip('\n') for line in file]
-    auth = tweepy.OAuth1UserHandler(*args)
-    return tweepy.API(auth)
+    return tweepy.OAuth1UserHandler(*args)
 
 
-def dump_tweets_data(id_lst, api):
-    """Dump tweets information into json file."""
+def dump_tweets_data(id_lst, auth):
+    """Dump tweets' id, retweet count and favorite count into json file.
+
+    :param id_lst: (list[str]) a list containing tweet id's.
+    :param auth: (tweepy.auth.OAuth1UserHandler) an auth object.
+    """
+    api = tweepy.API(auth, wait_on_rate_limit=True)
 
     def get_tweet_counts(tweet_id):
         """Return counts of favorites and retweets."""
         try:
             tweet = api.get_status(tweet_id)
+        except tweepy.TweepyException:
+            print(f'{tweet_id[:5]}... failed')
+            favorite_count = float('nan')
+            retweet_count = float('nan')
+        else:
+            print(f'{tweet_id[:5]}... successful')
             if hasattr(tweet, 'retweeted_status'):
                 favorite_count = tweet.retweeted_status.favorite_count
                 retweet_count = tweet.retweeted_status.retweet_count
             else:
                 favorite_count = tweet.favorite_count
                 retweet_count = tweet.retweet_count
-            print(f'succeeded to get data of id {tweet_id}')
-        except tweepy.TweepyException:
-            print(f'failed to get data of id {tweet_id}')
-            favorite_count = float('nan')
-            retweet_count = float('nan')
         return {'id': tweet_id, 'favorite_count': favorite_count,
                 'retweet_count': retweet_count}
 
@@ -48,7 +65,4 @@ def dump_tweets_data(id_lst, api):
 
 
 if __name__ == '__main__':
-    # download_file_from_url(url_path='link.txt', destination='image_predictions.tsv')
-    df = pd.read_csv('twitter-archive-enhanced.csv')
-    dump_tweets_data(id_lst=df['tweet_id'].astype(str),
-                     api=get_tweeter_api())
+    pass
